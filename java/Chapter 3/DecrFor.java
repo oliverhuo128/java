c2267224f7b6221f2d7227c67cb3c15be7a7027f
Oliver Huo <oliverhuo128@gmail.com>
2023-03-22 16:46:02 +0000

    java examples up unitl DecrFor loops

---
+++ java/Chapter 3/DecrFor.java
@@ -0,0 +1,11 @@
+// a negatively running for loop.
+
+class DecrFor {
+    public static void main(String[] args) {
+        int x;
+
+        for(x = 100; x > -100; x -= 5) {
+            System.out.println(x);
+        }
+    }
+}
