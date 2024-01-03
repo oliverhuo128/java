04def5b71337d2e43963940e4d01c5369a850045
Oliver Huo <oliverhuo128@gmail.com>
2024-01-03 23:47:19 +0000

    CallByValue.java

---
+++ java/Chapter6/CallByValue.java
@@ -0,0 +1,26 @@
+// Primitive types are passed by value
+class Test {
+    /* This method causes no change to the arguments
+     * used in the call.
+     */
+    void noChange(int i, int j) {
+        i = i + j;
+        j = -j;
+    }
+}
+
+class CallByValue {
+    public static void main(String[] args) {
+        Test ob = new Test();
+
+        int a = 15, b = 20;
+
+        System.out.println("a and b before call: " +
+        a + " " + b);
+
+        ob.noChange(a, b);
+
+        System.out.println("a and b after call: " +
+        a + " " + b);
+    }
+}
