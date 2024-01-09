9f0ff117821c823daee5b329b16bbe04fbd4664a
oliverhuo128 <oliverhuo128@gmail.com>
2024-01-09 23:38:38 +0000

    static demo2

---
+++ java/Chapter6/SDemo2.java
@@ -0,0 +1,22 @@
+// Use a static method
+class StaticMeth {
+    static int val = 1024; // a static variable
+
+    // a static method
+    static int valDiv2() {
+        return val/2;
+    }
+}
+
+class SDemo2 {
+    public static void main(String[] args) {
+
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " + StaticMeth.valDiv2());
+
+        StaticMeth.val = 4;
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDeiv2(): " +
+        StaticMeth.valDiv2());
+    }
+}
