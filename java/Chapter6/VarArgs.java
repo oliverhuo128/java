48307ae9e80129b474b4fe641b22743b543be310
oliverhuo128 <oliverhuo128@gmail.com>
2024-01-18 00:01:41 +0000

    varargs

--- java/Chapter6/VarArgs.java
+++ java/Chapter6/VarArgs.java
@@ -1,2 +1,23 @@
 // Demonstrate variable-length arguments.
-cla
+class VarArgs {
+
+    // vaTest() uses a vararg.
+    static void vaTest(int ... v) {
+        System.out.println("Number of args: " + v.length);
+        System.out.println("Contents: ");
+
+        for(int i=0; i < v.length; i++)
+            System.out.println(" arg " + i + ": " + v[i]);
+        
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Notice how vaTest() can be called with a
+        // variable number of arguments.
+
+        vaTest(10);     // 1 arg
+        vaTest(1, 2, 3); // 3 args
+        vaTest();            // no args
+    }
+}
