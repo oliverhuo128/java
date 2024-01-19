f6b820a53ab5dc1fbeabb6669f1b36fd28c985aa
zhenting.huo <oliverhuo128@gmail.com>
2024-01-19 16:14:50 +0000

    overloading varargs error

---
+++ java/Chapter6/VarArgs4.java
@@ -0,0 +1,21 @@
+// Varargs, overloading, and ambiguity.
+//
+// This program contains an error and will
+// not compile!
+class VarArgs4 {
+    // Use an int vararg parameter.
+    static void vaTest(int... v) {
+        // ...
+    }
+
+    // Use a boolean vararg parameter.
+    static void vaTest(boolean... v) {
+        // ...
+    }
+
+    public static void main(String[] args) {
+        vaTest(1, 2, 3);             // OK
+        vaTest(true, false, false);  // OK
+        vaTest();                    // Error: Ambiguous!
+    }
+}
