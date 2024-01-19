67c4f506ab471c324cbfde3130dfd8f96eeec198
zhenting.huo <oliverhuo128@gmail.com>
2024-01-19 17:33:51 +0000

    shapes and triangles inheritance

---
+++ java/Chapter7/Shapes2.java
@@ -0,0 +1,55 @@
+// Use accessor methods to set and get private members.
+// A class for two-dimensional objects.
+class TwoDShape {
+    private double width; // these are
+    private double height; // now private
+
+    // Accessor methods for width and height.
+    double getWidth() { return width; }
+    double getHeight() { return height; }
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+
+    void showDim() {
+        System.out.println("Width and height are " + width + " and " + height);
+    }
+}
+
+// A subclass of TwoDShape for triangles.
+class Triangle extends TwoDShape {
+    String style;
+
+    double area() {
+        return getWidth() * getHeight() / 2;
+    }
+
+    void showStyle() {
+        System.out.println("Triangle is " + style);
+    }
+}
+
+class Shapes2 {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        Triangle t2 = new Triangle();
+
+        t1.setWidth(4.0);
+        t1.setHeight(4.0);
+        t1.style = "filled";
+
+        t2.setWidth(8.0);
+        t2.setHeight(12.0);
+        t2.style = "outlined";
+
+        System.out.println("Info for t1: ");
+        t1.showStyle();
+        t1.showDim();
+        System.out.println("Area is " + t1.area());
+        System.out.println();
+
+        System.out.println("Info for t2: ");
+        t2.showStyle();
+        t2.showDim();
+        System.out.println("Area is " + t2.area());
+    }
+}
