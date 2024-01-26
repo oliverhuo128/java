91008606cac4f810629eeef387f61b7e264c1e1d
zhenting.huo <oliverhuo128@gmail.com>
2024-01-26 00:39:15 +0000

    finald

---
+++ java/Chapter7/FinalD.java
@@ -0,0 +1,32 @@
+// Return a String object.
+class ErrorMsg {
+    // Error codes.
+    final int OUTERR = 0;
+    final int INTERR = 1;
+    final int DISKERR = 2;
+    final int INDEXERR = 3;
+
+    String[] msgs = {
+        "Output Error",
+        "Input Error",
+        "Disk Full",
+        "Index Out-Of-Bounds"
+    };
+
+    // Return the error message.
+    String getErrorMsg(int i) {
+        if(i >= 0 & i < msgs.length)
+            return msgs[i];
+        else
+            return "Invalid Error Code";
+    }
+}
+
+class FinalD {
+    public static void main(String[] args) {
+        ErrorMsg err = new ErrorMsg();
+
+        System.out.println(err.getErrorMsg(err.OUTERR));
+        System.out.println(err.getErrorMsg(err.DISKERR));
+    }
+}
