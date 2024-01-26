7cee9063c7f015d8636bd427e768f5336a7659b8
zhenting.huo <oliverhuo128@gmail.com>
2024-01-26 00:45:22 +0000

    finald

--- java/Chapter7/FinalD.java
+++ java/Chapter7/FinalD.java
@@ -23,10 +23,10 @@ class ErrorMsg {
 }
 
 class FinalD {
-    public static void main(String[] args) {
+    final public static void main(String[] args) {
         ErrorMsg err = new ErrorMsg();
 
         System.out.println(err.getErrorMsg(err.OUTERR));
         System.out.println(err.getErrorMsg(err.DISKERR));
     }
-}
+} 
