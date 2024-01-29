f8bac03bad79c1162a9548fea90385953e23afa1
oliverhuo128 <oliverhuo128@gmail.com>
2024-01-29 23:44:02 +0000

    queue interface

--- java/Chapter8/QueueInterface/IQDemo/IQDemo.java
+++ java/Chapter8/QueueInterface/IQDemo/IQDemo.java
@@ -57,5 +57,134 @@ class CircularQueue implements ICharQ {
             }
 
             q[putloc++] = ch;
+            if(putloc==q.length) putloc = 0; // loop back
+    }
+
+    // Get a character from the queue.
+    public char get() {
+        if(getloc == putloc) {
+            System.out.println(" - Queue is empyt.");
+            return (char) 0;
+        }
+
+        char ch = q[getloc++];
+        if(getloc==q.length) getloc = 0; // loop back
+        return ch;
+    }
+}
+
+// A dynamic queue.
+class DynQueue implements ICharQ {
+    private char[] q;      // This array holds the queue
+    private int putloc, getloc;  // The put and get indices
+    
+    // Construct an empty queue given its size.
+    public DynQueue(int size) {
+        q = new char[size];  // Allocate memory for the queue
+        putloc = getloc = 0;
+    }
+    
+    // Put a character into the queue.
+    public void put(char ch) {
+        if (putloc == q.length) {
+            // Increase queue size
+            char[] t = new char[q.length * 2];
+            
+            // Copy elements into new queue
+            for (int i = 0; i < q.length; i++)
+                t[i] = q[i];
+            
+            q = t;
+        }
+        
+        q[putloc++] = ch;
+    }
+    
+    // Get a character from the queue.
+    public char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+        
+        return q[getloc++];
+    }
+}
+
+// Demonstrate the ICharQ interface.
+class IQDemo {
+    public static void main(String[] args) {
+        FixedQueue q1 = new FixedQueue(10);
+        DynQueue q2 = new DynQueue(5);
+        CircularQueue q3 = new CircularQueue(10);
+
+        ICharQ iQ;
+
+        char ch;
+        int i;
+
+        // Use fixed queue
+        iQ = q1;
+
+        // Put some characters into fixed queue.
+        for (i = 0; i < 10; i++)
+            iQ.put((char) ('A' + i));
+
+        // Show the Queue.
+        System.out.print("Contents of fixed queue: ");
+        for (i = 0; i < 10; i++) {
+            ch = iQ.get();
+            System.out.print(ch);
+        }
+        System.out.println();
+
+        // Use dynamic queue
+        iQ = q2;
+
+        // Put some characters into dynamic queue.
+        for (i = 0; i < 10; i++)
+            iQ.put((char) ('Z' - i));
+
+        // Show the queue.
+        System.out.print("Contents of dynamic queue: ");
+        for (i = 0; i < 10; i++) {
+            ch = iQ.get();
+            System.out.print(ch);
+        }
+        System.out.println();
+
+        // Use circular queue
+        iQ = q3;
+
+        // Put some characters into circular queue.
+        for (i = 0; i < 10; i++)
+            iQ.put((char) ('A' + i));
+
+        // Show the queue.
+        System.out.print("Contents of circular queue: ");
+        for (i = 0; i < 10; i++) {
+            ch = iQ.get();
+            System.out.print(ch);
+        }
+        System.out.println();
+
+        // Put more characters into circular queue.
+        for (i = 10; i < 20; i++)
+            iQ.put((char) ('A' + i));
+
+        // Show the queue.
+        System.out.print("Contents of circular queue: ");
+        for (i = 0; i < 10; i++) {
+            ch = iQ.get();
+            System.out.print(ch);
+        }
+        System.out.println("\nStore and consume from" + " circular queue.");
+
+        // Store in and consume from circular queue.
+        for (i = 0; i < 20; i++) {
+            iQ.put((char) ('A' + i));
+            ch = iQ.get();
+            System.out.print(ch);
+        }
     }
 }
