4730e5307b7d449bd574ff00ddf5035606a318fe
zhenting.huo <oliverhuo128@gmail.com*>
2024-07-12 16:15:40 +0100

    excdemo1 c1

--- java/Chapter9/ExcDemo1.java
+++ java/Chapter9/ExcDemo1.java
@@ -1,13 +1,14 @@
 // Demonstrate exception handling.
 class ExcDemo1 {
     public static void main(String[] args) {
-        int [] nums = new int [4];
-        try {
+        int[] nums = new int[4];
+
+        try{
             System.out.println("Before exception is generated.");
 
             // Generate an index out-of-bounds exception.
             nums[7] = 10;
-            System.out.println("this won't be dispalyed");
+            System.out.println("this won't be displayed");
         }
         catch (ArrayIndexOutOfBoundsException exc) {
             // catch the exception
