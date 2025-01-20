5a102b5a844590579f52b088c4af85e25bb43e1e
Oliver Huo <oliverhuo128@gmail.com>
2025-01-20 18:08:40 +0000

    showfile.java

---
+++ java/Chapter10/WriteDemo.java
@@ -0,0 +1,11 @@
+// Demostrate System.out.write().
+
+class WriteDemo {
+    public static void main(String[] args) {
+        int b;
+
+        b = 'X';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
