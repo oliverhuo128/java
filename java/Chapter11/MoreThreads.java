327ca933ef945db4e1ed00c9216f69e65716144f
Oliver Huo <oliverhuo128@gmail.com>
2025-08-04 20:34:07 +0100

    use is alive()

--- java/Chapter11/MoreThreads.java
+++ java/Chapter11/MoreThreads.java
@@ -32,22 +32,47 @@ class MyThread implements Runnable {
   }
 }
 
+// class MoreThreads {
+//   public static void main(String[] args) {
+//     System.out.println("Main thread starting.");
+//     MyThread mt1 = MyThread.createAndStart("Child #1");
+//     MyThread mt2 = MyThread.createAndStart("Child #2");
+//     MyThread mt3 = MyThread.createAndStart("Child #3");
+//
+//     for(int i=0; i < 50; i++) {
+//       System.out.print(".");
+//       try {
+//         Thread.sleep(100);
+//       }
+//       catch(InterruptedException exc) {
+//         System.out.println("Main thread interrupted.");
+//       }
+//     }
+//     System.out.println("Main thread ending.");
+//   }
+// }
+
+//Use isAlive().
 class MoreThreads {
   public static void main(String[] args) {
-    System.out.println("Main thread starting.");
-    MyThread mt1 = MyThread.createAndStart("Child #1");
-    MyThread mt2 = MyThread.createAndStart("Child #2");
-    MyThread mt3 = MyThread.createAndStart("Child #3");
+  System.out.println("Main thread starting.");
 
-    for(int i=0; i < 50; i++) {
-      System.out.print(".");
-      try {
-        Thread.sleep(100);
-      }
-      catch(InterruptedException exc) {
-        System.out.println("Main thread interrupted.");
-      }
+  MyThread mt1 = MyThread.createAndStart("Child #1");
+  MyThread mt2 = MyThread.createAndStart("Child #2");
+  MyThread mt3 = MyThread.createAndStart("Child #3");
+
+  do {
+    System.out.print(".");
+    try {
+      Thread.sleep(100);
     }
-    System.out.println("Main thread ending.");
+    catch(InterruptedException exc) {
+      System.out.println("Main thread interrupted.");
+    }
+  }while (mt1.thrd.isAlive() ||
+             mt2.thrd.isAlive() ||
+             mt3.thrd.isAlive());
+
+  System.out.println("Main thread ending.");
   }
 }
